3e74a9c585e72d2b5866af767b569281487e3c88
Biswajit Nag <biswajitnag68@gmail.com>
2021-11-30 10:45:28 +0530

    tourism-site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function change(m) {
+		// body...
+		var x=document.getElementById("change");
+		x.className+="changecolor1";
+		x.innerHTML=m;
+		}
+		function makebig() {
+			// body...
+			var big=document.getElementById('navbar');
+			big.className="bigg";
+		}
